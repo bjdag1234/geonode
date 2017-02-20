57e7903b469862e4f8afbd4a66a84c2793b68b5a
geonode-prod-user <noaddress@dream.upd.edu.ph>
2017-02-20 14:34:01 +0800

    geonode.settings.py added API_INCLUDE_REGIONS_COUNT = False

--- geonode/cephgeo/cart_utils.py
+++ geonode/cephgeo/cart_utils.py
@@ -1,6 +1,5 @@
-from changuito import CartProxy
 from changuito import models as cartmodels
-from changuito.proxy import ItemDoesNotExist
+from changuito.proxy import ItemDoesNotExist, CartProxy
 from geonode.cephgeo.models import CephDataObject, DataClassification
 from django.contrib import messages
 import utils
