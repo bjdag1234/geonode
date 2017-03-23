0eadab798248fa6634cfda25a06f0bcacd6ac9dd
Mapavia <mapavia@dream.upd.edu.ph>
2017-03-23 11:12:12 +0800

    geonode/api/views.py	changed local_url to https, build_absolute_uri gives http 			added trailing / before ? GET variables

--- geonode/api/views.py
+++ geonode/api/views.py
@@ -32,8 +32,9 @@ def api_combined(request, apiname):
     current_url = request.build_absolute_uri()
     tostrip = 'limit='+str(request.GET.get('limit'))+'&offset='+str(request.GET.get('offset'))
     apiquery = '?'.join(current_url.split('?')[1:]).replace(tostrip,'').rstrip('&').lstrip('&')
-    local_url = urljoin(current_url,'../')
-    urls_to_visit = [local_url,'https://lipad-fmc.dream.upd.edu.ph/']
+    #local_url = urljoin(current_url,'../')
+    local_url = 'https://'+request.META['HTTP_HOST']+'/'
+    urls_to_visit = [local_url,'https://lipad.dream.upd.edu.ph/']
     output = {}
     # output = []
     for each_url in urls_to_visit:
@@ -60,14 +61,15 @@ def api_combined(request, apiname):
 def api_autocomplete(request):
     current_url = request.build_absolute_uri()
     apiquery = '?'.join(current_url.split('?')[1:])
-    local_url = urljoin(current_url,'../')
-    urls_to_visit = [local_url,'https://lipad-fmc.dream.upd.edu.ph/']
+    #local_url = urljoin(current_url,'../')
+    local_url = 'https://'+request.META['HTTP_HOST']+'/'
+    urls_to_visit = [local_url,'https://lipad.dream.upd.edu.ph/']
     output = ''
 
     # output = []
     for each_url in urls_to_visit:
         # output.append (each_url + 'autocomplete/ResourceBaseAutocomplete/?' + apiquery)
-        response = urllib2.urlopen(each_url + 'autocomplete/ResourceBaseAutocomplete?' + apiquery)
+        response = urllib2.urlopen(each_url + 'autocomplete/ResourceBaseAutocomplete/?' + apiquery)
         data = response.read()
         if 'No matches found' not in data:
             output += data
