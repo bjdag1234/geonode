1859beb579616f782eec0863c814e6a370a53f6c
Mapavia <mapavia@dream.upd.edu.ph>
2017-03-28 14:35:40 +0800

    scripts/utils/reports/daily_download_count.py merged with prod

--- scripts/utils/reports/daily_download_count.py
+++ scripts/utils/reports/daily_download_count.py
@@ -6,20 +6,50 @@ from geonode.reports.models import DownloadCount, SUCLuzViMin, DownloadTracker
 from datetime import datetime, timedelta
 from geonode.layers.models import Layer
 from geonode.cephgeo.models import FTPRequest, FTPRequestToObjectIndex, DataClassification
+from geonode.people.models import Profile
 
+from osgeo import ogr
+from shapely.geometry import Polygon
+from shapely.wkb import loads
+from shapely.ops import cascaded_union
+
+global layer_count, source
 layer_count = {}
+source = ogr.Open(("PG:host={0} dbname={1} user={2} password={3}".format(settings.DATABASE_HOST,settings.DATASTORE_DB,settings.DATABASE_USER,settings.DATABASE_PASSWORD)))
 
-def get_luzvimin(iterate):
-    layer_query = Layer.objects.get(typename=iterate['typename'])
-    keyword_list = layer_query.keywords.values_list()
-    try:
-        for eachkeyword in keyword_list[0]:
-            luzvimin_query = SUCLuzViMin.objects.filter(suc=eachkeyword)[0]
-            luzvimin = luzvimin_query.luzvimin
-            break
-    except Exception as e:
-        print (str(iterate['typename']) + ' - ' + str(e))
+def get_SUC_using_gridref(abscissa, ordinate, _TILE_SIZE = 1000):
+    data = source.ExecuteSQL("select * from "+settings.PL1_SUC_MUNIS)
+    tile_ulp = "%s %s" % (abscissa, ordinate)
+    tile_dlp = "%s %s" % (abscissa, ordinate - _TILE_SIZE)
+    tile_drp = "%s %s" % (abscissa + _TILE_SIZE, ordinate - _TILE_SIZE)
+    tile_urp = "%s %s" % (abscissa + _TILE_SIZE, ordinate)
+    tilestr = "POLYGON ((%s, %s, %s, %s, %s))"% (tile_ulp, tile_dlp, tile_drp, tile_urp, tile_ulp)
+    data.SetSpatialFilter(ogr.CreateGeometryFromWkt(tilestr))
+    for feature in data:
+        return feature.GetField("SUC")
+
+def get_luzvimin(data):
+    if data['grid_ref']:#If FTP
+        east = int(data['grid_ref'].split('N')[0][1:])*1000
+        north = int(data['grid_ref'].split('N')[1])*1000
+        SUC = get_SUC_using_gridref(east,north)
+        try:
+            luzvimin = SUCLuzViMin.objects.filter(suc=SUC)[0].luzvimin
+        except:
+            luzvimin = "Luzvimin_others"
+    else:
         luzvimin = "Luzvimin_others"
+        try:
+            layer_query = Layer.objects.get(typename=data['typename'])
+        except:
+            return luzvimin
+        keyword_list = layer_query.keywords.names()
+        for eachkeyword in keyword_list:
+            try:
+                luzvimin = SUCLuzViMin.objects.filter(suc=eachkeyword)[0].luzvimin
+                break
+            except Exception as e:
+                print (layer_query.typename + ' - ' + str(e))
     return luzvimin
 
 def add_to_count(category, typename):
@@ -34,10 +64,9 @@ def add_to_count(category, typename):
             "ORTHO": 0,
             "SAR": 0,
             "Others": 0,
+            "Resource":0,
         }
-    if 'coverage' in typename:
-        layer_count[category]['Coverage'] += 1
-    elif 'fh' in typename:
+    if 'fh' in typename:
         layer_count[category]['FHM'] += 1
     elif 'dtm' in typename:
         layer_count[category]['DTM'] += 1
@@ -49,6 +78,14 @@ def add_to_count(category, typename):
         layer_count[category]['ORTHO'] += 1
     elif 'sar' in typename:
         layer_count[category]['SAR'] += 1
+    elif 'coverage' in typename:
+        layer_count[category]['Coverage'] += 1
+    elif 'dem' in typename:
+        layer_count[category]['Coverage'] += 1
+    elif 'mkp' in typename:
+        layer_count[category]['Coverage'] += 1
+    elif any(lidar2keyword in typename for lidar2keyword in ['aquaculture', 'mangroves', 'agrilandcover', 'agricoastlandcover', 'irrigation', 'streams', 'wetlands', 'trees', 'ccm', 'chm', 'agb', 'power']):
+        layer_count[category]['Resource'] += 1
     else:
         layer_count[category]['Others'] += 1
 def add_to_monthlyc(category):
@@ -63,57 +100,59 @@ def add_to_monthlyc(category):
             "ORTHO": 0,
             "SAR": 0,
             "Others": 0,
+            "Resource": 0,
         }
     layer_count[category]['Document'] += 1
 
-datetoappend = datetime.strptime((datetime.now()-timedelta(days=1)).strftime('%d-%m-%Y'),'%d-%m-%Y') #timedelta to start week count days from sunday; days=3 meaning week count if from wednesday to tuesday
-# auth_list = Action.objects.filter(verb='downloaded').order_by('timestamp')
-auth_list = DownloadTracker.objects.order_by('timestamp')
-for auth in auth_list:
-    if datetoappend == datetime.strptime(auth.timestamp.strftime('%d-%m-%Y'),'%d-%m-%Y') and not auth.resource_type == 'document':#if datenow is timestamp
-        luzvimin = get_luzvimin({
-            "timestamp": auth.timestamp,
-            "typename": auth.title,
-            })
-        add_to_count(luzvimin, auth.title)
-        add_to_count('monthly', auth.title)
-    elif datetoappend == datetime.strptime(auth.timestamp.strftime('%d-%m-%Y'),'%d-%m-%Y') and auth.resource_type == 'document':#if datenow is timestamp
-        add_to_monthlyc('monthly')
+def main(minusdays, query_objects, attr_date, attr_actor, attr_type, attr_filename, FTP):
+    datetoanalyze = datetime.strptime((datetime.now()-timedelta(days=minusdays)).strftime('%d-%m-%Y'),'%d-%m-%Y') #if minusdays = 1, analyzes downloads day before; because python code is ran early morning
+    for each_object in query_objects.order_by(attr_date):
+        if datetoanalyze == datetime.strptime(getattr(each_object, attr_date).strftime('%d-%m-%Y'),'%d-%m-%Y'):
+            if attr_actor:
+                getprofile = Profile.objects.get(username=getattr(each_object,attr_actor))
+                if not getprofile.is_staff and not any('test' in var for var in [str(getattr(each_object,attr_actor)),getprofile.first_name,getprofile.last_name]):
+                    if FTP:
+                        type_list = FTPRequestToObjectIndex.objects.filter(ftprequest=each_object.id)
+                        for eachtype in type_list:
+                            FTPtype = DataClassification.gs_feature_labels[eachtype.cephobject._enum_data_class].lower()
+                            luzvimin = get_luzvimin({
+                                "grid_ref": eachtype.cephobject.grid_ref,
+                                })
+                            add_to_count(luzvimin, FTPtype)
+                            add_to_count('monthly', FTPtype)
+                    elif getattr(each_object,attr_type) == 'dataset' or not getattr(each_object,attr_type): #for DownloadTracker(if==dataset) or AnonDownloader(if not empty) therefore layer
+                        luzvimin = get_luzvimin({
+                            "typename": getattr(each_object,attr_filename),
+                            "grid_ref": False
+                            })
+                        add_to_count(luzvimin, getattr(each_object,attr_filename))
+                        add_to_count('monthly', getattr(each_object,attr_filename))
+                    else: #else document
+                        add_to_monthlyc('monthly')
 
-anon_list = AnonDownloader.objects.all().order_by('date')
-for anon in anon_list:
-    if datetoappend == datetime.strptime(anon.date.strftime('%d-%m-%Y'),'%d-%m-%Y') and not anon.anon_document:#if datenow is timestamp
-        luzvimin = get_luzvimin({
-            "timestamp": anon.date,
-            "typename": anon.anon_layer.typename,
-            })
-        add_to_count(luzvimin, anon.anon_layer.typename)
-        add_to_count('monthly', anon.anon_layer.typename)
-    elif datetoappend == datetime.strptime(anon.date.strftime('%d-%m-%Y'),'%d-%m-%Y') and anon.anon_document:#if datenow is timestamp
-        add_to_monthlyc('monthly')
-ftp_list = FTPRequest.objects.all().order_by('date_time')
-for ftp in ftp_list:
-    if datetoappend == datetime.strptime(ftp.date_time.strftime('%d-%m-%Y'),'%d-%m-%Y'):
-        getprofile_ftp = Profile.objects.get(username=ftp.user)
-        if not getprofile_ftp.is_staff and not any('test' in var for var in [str(ftp.user), getprofile_ftp.first_name, getprofile_ftp.last_name]):
-            type_list = FTPRequestToObjectIndex.objects.filter(ftprequest=ftp.id)
-            for eachtype in type_list:
-                add_to_count('monthly', DataClassification.gs_feature_labels[eachtype.cephobject._enum_data_class].lower())
-print(layer_count)
+def save_to_dc(minusdays,count_dict):
+    datetoanalyze = datetime.strptime((datetime.now()-timedelta(days=minusdays)).strftime('%d-%m-%Y'),'%d-%m-%Y')
+    for category, eachdict in count_dict.iteritems():
+        if category == 'Luzon' or category == 'Visayas' or category == 'Mindanao' or category == 'Luzvimin_others':
+            chart_group = 'luzvimin'
+        elif category == 'monthly':
+            chart_group = 'monthly'
+        for eachtype, eachvalue in eachdict.iteritems():
+            if eachvalue:
+                model_object = DownloadCount(date=str(datetoanalyze),
+                                            category=str(category),
+                                            chart_group=str(chart_group),
+                                            download_type=str(eachtype),
+                                            count=str(eachvalue))
+                model_object.save()
+                print str(datetoanalyze) +'-'+ str(category) +'-'+ str(chart_group) +'-'+ str(eachtype) +'-'+ str(eachvalue)
 
+if __name__ == "__main__":
+    minusdays = 1
+    layer_count = {}
+    main(minusdays,DownloadTracker.objects, 'timestamp', 'actor','resource_type','title', False)
+    main(minusdays,AnonDownloader.objects, 'date', False,'anon_document','anon_layer', False)
+    main(minusdays,FTPRequest.objects,'date_time','user','','',True)
+    print(layer_count)
 
-for eachkey, eachdict in layer_count.iteritems():
-    category = eachkey
-    if category == 'Luzon' or category == 'Visayas' or category == 'Mindanao' or category == 'Luzvimin_others':
-        chart_group = 'luzvimin'
-    elif category == 'monthly':
-        chart_group = 'monthly'
-    for eachtype, eachvalue in eachdict.iteritems():
-        if eachvalue:
-            model_object = DownloadCount(date=str(datetoappend),
-                                        category=str(category),
-                                        chart_group=str(chart_group),
-                                        download_type=str(eachtype),
-                                        count=str(eachvalue))
-            model_object.save()
-            print str(datetoappend) +'-'+ str(category) +'-'+ str(chart_group) +'-'+ str(eachtype) +'-'+ str(eachvalue)
+    save_to_dc(minusdays,layer_count)
