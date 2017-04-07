54ce8232856aec2eb0476be3b823b55875bc81a6
geonode-prod-user <noaddress@dream.upd.edu.ph>
2017-04-07 20:11:48 +0800

    fixed merge conflict

--- geonode/cephgeo/views.py
+++ geonode/cephgeo/views.py
@@ -27,16 +27,21 @@ import time
 import operator
 import json
 from geonode.cephgeo.utils import get_cart_datasize
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.core.urlresolvers import reverse
 from geonode.maptiles.models import SRS
 from django.utils.text import slugify
 
-from geonode.tasks.update import update_fhm_metadata_task, style_update, seed_layers, pl2_metadata_update, sar_metadata_update, layer_default_style
+from geonode.tasks.update import style_update, seed_layers, pl2_metadata_update, sar_metadata_update, layer_default_style, job_result_task
+from geonode.tasks.fhm_metadata import update_fhm_metadata_task
 from geonode.base.enumerations import CHARSETS
 
 from geonode import settings
 from geonode.layers.models import Layer
+
+# celery  multiprocessing
+from celery import group
+from django.db.models import Q
 
 # Create your views here.
 
@@ -461,8 +466,25 @@ def management(request):
 @user_passes_test(lambda u: u.is_superuser)
 def update_fhm_metadata(request):
     # fhm_metadata_update.delay()
-    update_fhm_metadata_task.delay()
-    messages.error(request, "Updating Flood Hazard Map metadata")
+    layer_list = []
+    # layer_list = Layer.objects.filter(
+    #     Q(name__iregex=r'^ph[0-9]+_fh')).order_by('-upload_session')
+    layer_list = Layer.objects.filter(
+        Q(name__iregex=r'^ph[0-9]+_fh')).order_by('-upload_session')[:10]
+
+    # compute start time of update
+    start_time = datetime.now()
+
+    # get primary key of layer to pass in the task
+    jobs = group(update_fhm_metadata_task.s(l.pk) for l in layer_list).apply_async()
+    job_result = jobs.apply_async()
+
+    # mali to
+    # job_result_task.delay(job_result, start_time)
+
+    # result = chord(update_fhm_metadata_task.s(l.pk) for l in layer_list)(job_result_task.s())
+
+    messages.error(request, "Updating FHM Metadata")
     return HttpResponseRedirect(reverse('data_management'))
 
 
