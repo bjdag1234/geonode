ffb9e5232118b90a13db30f5c1d0ac54dbbcb1c2
Denise Leviste <drleviste@gmail.com>
2017-04-07 22:23:19 +0800

    Added line in tagging SUC

--- geonode/tasks/fhm_metadata.py
+++ geonode/tasks/fhm_metadata.py
@@ -3,6 +3,7 @@ import geonode.settings as settings
 from celery.task import task
 from geonode.layers.models import Layer
 from layer_metadata import update_metadata
+from delete_layers import delete_
 from tag_layers import update_tags
 
 # metadata update, seeding, SUC/FP tagging
@@ -10,10 +11,18 @@ from tag_layers import update_tags
 def update_fhm_metadata_task(pk):
     layer = Layer.objects.get(pk=pk)
     update_metadata(layer)
+    mode = 'fhm'
+    update_tags(layer, mode)
 
+# delete layer (geoserver, geonode+postgis), defeault style
+@task(name='geonode.tasks.fhm_metadata.delete_fhm_task', queue='fhm_metadata')
+def delete_fhm_task(pk):
+    layer = Layer.objects.get(pk=pk)
+    delete_(layer)
 
 @task(name='geonode.tasks.fhm_metadata.tag_fhm_task', queue='fhm_metadata')
 def tag_fhm_task(pk):
     layer = Layer.objects.get(pk=pk)
     mode = 'fhm'
     update_tags(layer, mode)
+
