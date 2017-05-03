d0aeb2c58a4577bf840416fd6035b54891314ece
Ken Salanio <kssalanio@gmail.com>
2017-05-03 12:20:44 +0800

    added hostname and os for automation tasks

--- geonode/automation/models.py
+++ geonode/automation/models.py
@@ -11,8 +11,16 @@ class AutomationJob(models.Model):
     status = models.CharField(max_length=50, blank=False, null=False)
     turnover_id = models.CharField(max_length=10, blank=False, null=False)
     log = models.TextField(null=False,blank=True)
+    target_host = models.CharField(max_length=50, blank=False, null=False) 
+    target_os = models.CharField(max_length=20, blank=False, null=False) 
 
     def __unicode__(self):
         return "{0} {1} {2} {3}". \
             format(self.datatype, self.pathname,
                    self.date_submitted, self.status)
+
+class AutomationMachine(models.Model):
+    hostname = models.CharField(max_length=50, blank=False, null=False) 
+    ip_address = models.CharField(max_length=20, blank=False, null=False)
+    os = models.CharField(max_length=20, blank=False, null=False)
+    
