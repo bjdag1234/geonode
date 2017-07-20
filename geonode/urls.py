6f468dbb41b2192ed5f219631db39789190c57cc
Denise Leviste <drleviste@gmail.com>
2017-07-20 15:47:42 +0800

    Url formatting, added admin docs

--- geonode/urls.py
+++ geonode/urls.py
@@ -35,7 +35,7 @@ import django_cas_ng
 import autocomplete_light
 from geonode.views import forbidden
 
-#from geonode import settings
+# from geonode import settings
 
 # Setup Django Admin
 autocomplete_light.autodiscover()
@@ -57,7 +57,8 @@ urlpatterns = patterns('',
                        # Static pages
                        url(r'^/?$', 'geonode.views.philgrid', name='home'),
                        url(r'^help/$', TemplateView.as_view(template_name='help.html'), name='help'),
-                       url(r'^developer/$', TemplateView.as_view(template_name='developer.html'), name='developer'),
+                       url(r'^developer/$', TemplateView.as_view(
+                           template_name='developer.html'), name='developer'),
                        url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 
                        # EULA URLs
@@ -76,10 +77,12 @@ urlpatterns = patterns('',
                        (r'^catalogue/', include('geonode.catalogue.urls')),
 
                        # data.json
-                       url(r'^data.json$', 'geonode.catalogue.views.data_json', name='data_json'),
+                       url(r'^data.json$', 'geonode.catalogue.views.data_json',
+                           name='data_json'),
 
                        # Search views
-                       url(r'^search/$', TemplateView.as_view(template_name='search/search.html'), name='search'),
+                       url(r'^search/$', TemplateView.as_view(
+                           template_name='search/search.html'), name='search'),
 
                        # Social views
                        #(r"^account/", include("account.urls")),
@@ -92,46 +95,64 @@ urlpatterns = patterns('',
                        (r'^messages/', include('user_messages.urls')),
                        (r'^social/', include('geonode.social.urls')),
                        (r'^security/', include('geonode.security.urls')),
-                       url(r'^register/$','geonode.datarequests.views.register', name='register'),
+                       url(r'^register/$',
+                           'geonode.datarequests.views.register', name='register'),
 
                        # Accounts
-                       #url(r'^account/ajax_login$', 'geonode.views.ajax_login', name='account_ajax_login'),
-                       url(r'^account/login$', 'django_cas_ng.views.login', name='account_ajax_login'),
-                       url(r'^account/login/$','django_cas_ng.views.login', name='account_login'),
-                       url(r'^account/login/$', 'django_cas_ng.views.login', name='cas_ng_login'),
-                       url(r'^account/logout/$', 'django_cas_ng.views.logout', name='cas_ng_logout'),
-                       url(r'^account/logout/$', 'django_cas_ng.views.logout', name='account_logout'),
-                       url(r'^accounts/callback$', 'django_cas_ng.views.callback', name='cas_ng_proxy_callback'),
-                       url(r'^account/ajax_lookup$', 'geonode.views.ajax_lookup', name='account_ajax_lookup'),
+                       # url(r'^account/ajax_login$',
+                       # 'geonode.views.ajax_login',
+                       # name='account_ajax_login'),
+                       url(r'^account/login$', 'django_cas_ng.views.login',
+                           name='account_ajax_login'),
+                       url(r'^account/login/$',
+                           'django_cas_ng.views.login', name='account_login'),
+                       url(r'^account/login/$',
+                           'django_cas_ng.views.login', name='cas_ng_login'),
+                       url(r'^account/logout/$',
+                           'django_cas_ng.views.logout', name='cas_ng_logout'),
+                       url(r'^account/logout/$',
+                           'django_cas_ng.views.logout', name='account_logout'),
+                       url(r'^accounts/callback$', 'django_cas_ng.views.callback',
+                           name='cas_ng_proxy_callback'),
+                       url(r'^account/ajax_lookup$', 'geonode.views.ajax_lookup',
+                           name='account_ajax_lookup'),
 
-		       #Geocoding
-                       (r'^geocoding/',include('geonode.arealocate.urls',namespace='arealocate')),
+                       # Geocoding
+                       (r'^geocoding/', include('geonode.arealocate.urls',
+                                                namespace='arealocate')),
 
                        # Data Request Profiles
-                       (r'^requests/', include('geonode.datarequests.urls', namespace='datarequests')),
+                       (r'^requests/', include('geonode.datarequests.urls',
+                                               namespace='datarequests')),
 
                        # Misc
                        # url(r'^captcha/', include('captcha.urls')),
 
-					   # CephGeo
-					   url(r'^ceph/', include("geonode.cephgeo.urls")),
+                       # CephGeo
+                       url(r'^ceph/',
+                           include("geonode.cephgeo.urls")),
 
-             # Automation
-             url(r'^automation/', include('geonode.automation.urls', namespace='automation')),
+                       # Automation
+                       url(r'^automation/', include('geonode.automation.urls',
+                                                    namespace='automation')),
 
-					   #MapTiles
-					   url(r'^maptiles/',include("geonode.maptiles.urls")),
+                       # MapTiles
+                       url(r'^maptiles/', include("geonode.maptiles.urls")),
 
                        # Meta
                        url(r'^lang\.js$', TemplateView.as_view(template_name='lang.js', content_type='text/javascript'),
                            name='lang'),
 
-                       url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict, name='jscat'),
+                       url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog',
+                           js_info_dict, name='jscat'),
                        url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps},
                            name='sitemap'),
 
                        (r'^i18n/', include('django.conf.urls.i18n')),
                        (r'^autocomplete/', include('autocomplete_light.urls')),
+                       # django-admin documentation
+                       (r'^admin/doc/',
+                        include('django.contrib.admindocs.urls')),
                        (r'^admin/', include(admin.site.urls)),
                        (r'^groups/', include('geonode.groups.urls')),
                        (r'^documents/', include('geonode.documents.urls')),
@@ -176,6 +197,8 @@ handler403 = 'geonode.views.err403'
 
 # Featured Maps Pattens
 urlpatterns += patterns('',
-                        (r'^featured/(?P<site>[A-Za-z0-9_\-]+)/$', 'geonode.maps.views.featured_map'),
-                        (r'^featured/(?P<site>[A-Za-z0-9_\-]+)/info$', 'geonode.maps.views.featured_map_info'),
+                        (r'^featured/(?P<site>[A-Za-z0-9_\-]+)/$',
+                         'geonode.maps.views.featured_map'),
+                        (r'^featured/(?P<site>[A-Za-z0-9_\-]+)/info$',
+                         'geonode.maps.views.featured_map_info'),
                         )
