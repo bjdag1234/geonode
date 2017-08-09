563146bb21740bb1115d7d054c92272df582c5f7
Denise Leviste <drleviste@gmail.com>
2017-08-09 15:19:01 +0800

    Module for inserting in CephDataObjectResourceBase

--- geonode/tasks/ceph_update.py
+++ geonode/tasks/ceph_update.py
@@ -10,16 +10,50 @@ import geonode.settings as settings
 from geonode.cephgeo.utils import get_data_class_from_filename
 from geonode.cephgeo import ceph_client
 from swiftclient.exceptions import ClientException
+
+from geonode.automation.models import AutomationJob, CephDataObjectResourceBase
+from geonode.cephgeo.models import LidarCoverageBlock
+from celery.decorators import periodic_task
+
 logger = get_task_logger("geonode.tasks.ceph_update")
 
 
+def transform_log_to_list(log):
+    log_list = []
+    log = log.strip()
+    log_list = log.split('\r\n')
+
+    # print 'Log List:', log_list
+    logger.info('Cleaned log.')
+
+    return log_list
+
+
+def get_uid_from_filename(filename):
+    name = filename.split('.')[0]
+    uid = int((name.split('_')[3].split('U')[-1:]).pop(0))
+    print 'UID', uid
+
+    block_object = LidarCoverageBlock.objects.get(uid=uid)
+
+    return block_object
+
+
 @task(name='geonode.tasks.ceph_update.ceph_metadata_update', queue='update')
-def ceph_metadata_update(uploaded_objects_list, update_grid=True):
+def ceph_metadata_update():
     """
         NOTE: DOES NOT WORK
           Outputs error 'OperationalError: database is locked'
           Need a better way of making celery write into the database
     """
+    print '#' * 10
+    print 'Scheduler'
+    print '#' * 10
+
+    #: Get from AutomationJob Mode
+    job = AutomationJob.objects.get(status=AutomationJob.STATUS_CHOICES.done_ceph)
+    uploaded_objects_list = transform_log_to_list(job.ceph_upload_log)
+
     # Pop first line containing header
     uploaded_objects_list.pop(0)
     """NAME,LAST_MODIFIED,SIZE_IN_BYTES,CONTENT_TYPE,GEO_TYPE,FILE_HASH GRID_REF"""
@@ -33,7 +67,7 @@ def ceph_metadata_update(uploaded_objects_list, update_grid=True):
         len(uploaded_objects_list)))
     for ceph_obj_metadata in uploaded_objects_list:
         metadata_list = ceph_obj_metadata.split(csv_delimiter)
-        #logger.info("-> {0}".format(ceph_obj_metadata))
+        # logger.info("-> {0}".format(ceph_obj_metadata))
         # Check if metadata list is valid
         if len(metadata_list) is 6:
             # try:
@@ -41,8 +75,12 @@ def ceph_metadata_update(uploaded_objects_list, update_grid=True):
                 Retrieve and check if metadata is present, update instead if there is
             """
             ceph_obj = None
+            for x in metadata_list:
+                print 'Metadata', x
+
+            ceph_obj = None
             try:
-                ceph_obj = CephDataObject.objects.get(name=metadata_list[0])
+                ceph_obj = CephDataObjectResourceBase.objects.get(name=metadata_list[0])
                 # Commented attributes are not relevant to update
                 # ceph_obj.grid_ref = metadata_list[5]
                 # ceph_obj.data_class = get_data_class_from_filename(metadata_list[0])
@@ -56,15 +94,16 @@ def ceph_metadata_update(uploaded_objects_list, update_grid=True):
 
                 objects_updated += 1
             except ObjectDoesNotExist:
-                ceph_obj = CephDataObject(name=metadata_list[0],
-                                          #last_modified = time.strptime(metadata_list[1], "%Y-%m-%d %H:%M:%S"),
-                                          last_modified=metadata_list[1],
-                                          size_in_bytes=metadata_list[2],
-                                          content_type=metadata_list[3],
-                                          data_class=get_data_class_from_filename(
-                                              metadata_list[0]),
-                                          file_hash=metadata_list[4],
-                                          grid_ref=metadata_list[5])
+                ceph_obj = CephDataObjectResourceBase(name=metadata_list[0],
+                                                      last_modified=metadata_list[1],
+                                                      size_in_bytes=metadata_list[2],
+                                                      content_type=metadata_list[3],
+                                                      data_class=get_data_class_from_filename(
+                    metadata_list[0]),
+                    file_hash=metadata_list[4],
+                    grid_ref=metadata_list[5],
+                    block_uid=get_uid_from_filename(
+                    metadata_list[0]))
                 ceph_obj.save()
 
                 objects_inserted += 1
@@ -85,10 +124,11 @@ def ceph_metadata_update(uploaded_objects_list, update_grid=True):
     # Pass to celery the task of updating the gird shapefile
     result_msg = "Succesfully encoded metadata of [{0}] of objects. Inserted [{1}], updated [{2}].".format(
         objects_inserted + objects_updated, objects_inserted, objects_updated)
-    if update_grid:
-        result_msg += " Starting feature updates for PhilGrid shapefile."
-        grid_feature_update.delay(gridref_dict_by_data_class)
-    print result_msg
+
+    # if update_grid:
+    #     result_msg += " Starting feature updates for PhilGrid shapefile."
+    #     grid_feature_update.delay(gridref_dict_by_data_class)
+    # print result_msg
 
 
 @task(name='geonode.tasks.ceph_update.ceph_metadata_remove', queue='update')
@@ -99,19 +139,19 @@ def ceph_metadata_remove(uploaded_objects_list, update_grid=True, delete_from_ce
     # Pop first line containing header
     uploaded_objects_list.pop(0)
     """NAME,LAST_MODIFIED,SIZE_IN_BYTES,CONTENT_TYPE,GEO_TYPE,FILE_HASH GRID_REF"""
-    print "Update grid: ["+str(update_grid)+"] Delete Ceph Objects: ["+str(delete_from_ceph)+"]"
+    print "Update grid: [" + str(update_grid) + "] Delete Ceph Objects: [" + str(delete_from_ceph) + "]"
 
     # Loop through each metadata element
-    csv_delimiter=','
-    objects_deleted=0
-    objects_not_found=0
-    gridref_dict_by_data_class=dict()
+    csv_delimiter = ','
+    objects_deleted = 0
+    objects_not_found = 0
+    gridref_dict_by_data_class = dict()
     logger.info("Removing {0} ceph data objects".format(len(uploaded_objects_list)))
-    
+
     # Create ceph connection
     cephclient = ceph_client.CephStorageClient(settings.CEPH_OGW['default']['USER'], settings.CEPH_OGW[
-                                          'default']['KEY'], settings.CEPH_OGW['default']['LOCATION'])
-    
+        'default']['KEY'], settings.CEPH_OGW['default']['LOCATION'])
+
     for ceph_obj_metadata in uploaded_objects_list:
         metadata_list = ceph_obj_metadata.split(csv_delimiter)
         logger.info("-> {0}".format(ceph_obj_metadata))
@@ -166,3 +206,9 @@ def grid_feature_update(gridref_dict_by_data_class, field_value=1):
         logger.info("Updating feature attribute [{0}]".format(feature_attr))
         nested_grid_update(grid_ref_list, feature_attr, field_value)
         logger.info("Finished task for feature [{0}]".format(feature_attr))
+
+
+# @task
+# def setup_periodic_tasks(sender, **kwargs):
+
+#     sender.add_periodic_task(60.0, ceph_metadata_update(), name='Automation Model Update')
