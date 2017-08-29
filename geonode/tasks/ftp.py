4fc323156e813b24824d47e32376054a84c06218
Kenneth V. Langga <klangga@gmail.com>
2017-08-29 14:14:54 +0800

    Specify env.shell

--- geonode/tasks/ftp.py
+++ geonode/tasks/ftp.py
@@ -22,6 +22,7 @@ logger = logging.getLogger("geonode.tasks.ftp")
 FTP_USERS_DIRS = {"test-ftp-user": "/mnt/ftp_pool/FTP/PL1/testfolder", }
 env.skip_bad_hosts = True
 env.warn_only = True
+env.shell = '/usr/local/bin/bash -l -c'
 
 
 class UserEmptyException(Exception):
